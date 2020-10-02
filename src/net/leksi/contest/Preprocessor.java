30c33df2de85ab14411dca3d5107052382eff178
Alexey Zakharov <leksi@leksi.net>
2020-10-02 13:50:52 +0300

    Initial commit

--- src/net/leksi/contest/Preprocessor.java
+++ src/net/leksi/contest/Preprocessor.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Alexey Zakharov <leksi@leksi.net>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package net.leksi.contest;
 
 import java.io.BufferedReader;
@@ -24,7 +47,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Preprocessor {
+class Preprocessor {
     
     enum WaitingFor {COMPILED, CLASS, METHOD_OR_FIELD, METHOD, FIELD, CODE, OPER, EXCEPTION_TABLE, TABLE_SWITCH, DONE, NONE};
     
@@ -45,8 +68,6 @@ public class Preprocessor {
         }
     }
     
-    private String[] ext_projects = null;
-    
     private boolean debug = false;
     private String skipPrefix = null;
     
@@ -58,14 +79,7 @@ public class Preprocessor {
         this.skipPrefix = skipPrefix;
     }
 
-    
-    public Preprocessor() {
-        ext_projects = 
-                Arrays.stream(((URLClassLoader) (Thread.currentThread().getContextClassLoader())).getURLs()).
-                        map(v -> v.toString()).toArray(i -> new String[i]);
-    }
-    
-    class JavaSource {
+    static class JavaSource {
         File file;
         JarFile jarFile;
         JarEntry jarEntry;
@@ -185,7 +199,7 @@ public class Preprocessor {
                                     wf[0] = WaitingFor.OPER;
                                     if(debug) { System.out.println(INDENTION + wf[0] + " OK" + (skip ? " (skipped)" : "")); }
                                 }
-                                if(debug) { System.out.println("input: " + line); }
+                                if(debug) { System.out.println("javap: " + line); }
                                 if(line.isEmpty()) {
                                     if(wf[0] != WaitingFor.TABLE_SWITCH && wf[0] != WaitingFor.NONE) {
                                         wf[0] = WaitingFor.METHOD_OR_FIELD;
